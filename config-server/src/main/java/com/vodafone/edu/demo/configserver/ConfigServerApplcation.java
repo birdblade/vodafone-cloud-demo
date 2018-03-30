02d94f6d7bdf1c80f4f3bd62b17db56cb4c2e016
mkarakas <murat@eteration.com>
2018-03-30 12:11:51 +0300

    java files

---
+++ config-server/src/main/java/com/vodafone/edu/demo/configserver/ConfigServerApplcation.java
@@ -0,0 +1,16 @@
+package com.vodafone.edu.demo.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplcation {
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplcation.class, args);
+	}
+
+}
