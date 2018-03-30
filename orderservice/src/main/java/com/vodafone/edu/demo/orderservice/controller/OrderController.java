845922792bbe3d1622f4ef413e8f7294b3c5d18a
mkarakas <murat@eteration.com>
2018-03-30 16:47:03 +0300

    client added

--- orderservice/src/main/java/com/vodafone/edu/demo/orderservice/controller/OrderController.java
+++ orderservice/src/main/java/com/vodafone/edu/demo/orderservice/controller/OrderController.java
@@ -1,17 +1,23 @@
 package com.vodafone.edu.demo.orderservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vodafone.edu.demo.orderservice.client.ProductServiceClient;
 import com.vodafone.edu.demo.orderservice.dto.OrderDTO;
 import com.vodafone.edu.demo.orderservice.dto.OrderResult;
+import com.vodafone.edu.demo.orderservice.dto.ProductDTO;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
 	
+	
+	@Autowired
+	private ProductServiceClient productServiceClient; 
 	
 	
 	@RequestMapping(method = RequestMethod.POST)
@@ -20,7 +26,8 @@ public class OrderController {
 		OrderResult or = new OrderResult();
 		
 		or.setMessage("Dear customer "+order.getCustomer() +" your order is send to your address: "+order.getAddress());
-		double unitPrice = 50;
+		ProductDTO productDTO = productServiceClient.readProduct(order.getProductId());
+		double unitPrice = productDTO.getPrice();
 		or.setTotalAmount(unitPrice*order.getCount());
 		
 		return or;
